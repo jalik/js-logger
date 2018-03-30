c2d9ac671033122b48fd41bb078ee22447354b27
jalik <karl.stein.pro@gmail.com>
2018-03-29 18:52:54 -1000

    Adds gulp task `eslint` Reformats code using `airbnb` eslint preset Removes `test/index.test.js` Removes `src/index.js`

--- src/logger.js
+++ src/logger.js
@@ -15,7 +15,7 @@
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
@@ -23,187 +23,202 @@
  *
  */
 
-import Observer from "@jalik/observer";
-import Types from "./types";
-import {extendRecursively} from "@jalik/extend";
+import { extendRecursively } from '@jalik/extend';
+import Observer from '@jalik/observer';
+import Types from './types';
 
-export class Logger {
+class Logger {
+  constructor(options) {
+    this.options = extendRecursively({
+      active: true,
+      console: {
+        debug: true,
+        error: true,
+        info: true,
+        other: true,
+        warning: true,
+      },
+      displayContext: false,
+    }, options);
 
-    constructor(options) {
-        this.options = extendRecursively({
-            active: true,
-            console: {
-                debug: true,
-                error: true,
-                info: true,
-                other: true,
-                warning: true
-            },
-            displayContext: false
-        }, options);
+    // Create observer
+    this.observer = new Observer(this);
 
-        // Create observer
-        this._observer = new Observer(this);
+    // Check console availability
+    if (typeof console !== 'object' || console === null) {
+      throw new Error('The console object is not available in this environment.');
+    }
 
-        // Check console availability
-        if (typeof console !== "object" || console === null) {
-            throw new Error("The console object is not available in this environment.");
+    // Add polyfill methods to the console object
+    // eslint-disable-next-line
+    if (typeof console.log === 'function') {
+      // eslint-disable-next-line
+      if (typeof console.debug !== 'function') {
+        // eslint-disable-next-line
+        console.debug = console.log;
+      }
+      // eslint-disable-next-line
+      if (typeof console.error !== 'function') {
+        // eslint-disable-next-line
+        console.error = console.log;
+      }
+      // eslint-disable-next-line
+      if (typeof console.info !== 'function') {
+        // eslint-disable-next-line
+        console.info = console.log;
+      }
+      // eslint-disable-next-line
+      if (typeof console.warn !== 'function') {
+        // eslint-disable-next-line
+        console.warn = console.log;
+      }
+    }
+  }
+
+  /**
+   * Logs a debug message
+   * @param message
+   * @param context
+   */
+  debug(message, context) {
+    return this.log(message, Types.debug, context);
+  }
+
+  /**
+   * Logs an error message
+   * @param messageOrError
+   * @param context
+   */
+  error(messageOrError, context) {
+    // eslint-disable-next-line
+    const ctx = context || {};
+    let msg = messageOrError;
+
+    if (messageOrError instanceof Error) {
+      // eslint-disable-next-line
+      ctx.error = {};
+
+      const attributes = ['name', 'message', 'reason', 'stack', 'type'];
+
+      for (let i = 0; i < attributes.length; i += 1) {
+        if (attributes[i] in messageOrError) {
+          // eslint-disable-next-line
+          ctx.error[attributes[i]] = messageOrError[attributes[i]];
         }
+      }
+      // eslint-disable-next-line
+      const { message } = messageOrError;
+      msg = message;
+    }
+    return this.log(msg, Types.error, ctx);
+  }
 
-        // Add polyfill methods to the console object
-        if (typeof console.log === "function") {
-            if (typeof console.debug !== "function") {
-                console.debug = console.log;
-            }
-            if (typeof console.error !== "function") {
-                console.error = console.log;
-            }
-            if (typeof console.info !== "function") {
-                console.info = console.log;
-            }
-            if (typeof console.warn !== "function") {
-                console.warn = console.log;
-            }
-        }
+  /**
+   * Logs an information message
+   * @param message
+   * @param context
+   */
+  info(message, context) {
+    return this.log(message, Types.info, context);
+  }
+
+  /**
+   * Checks if the logger is active
+   * @return {boolean}
+   */
+  isActive() {
+    return this.options.active === true;
+  }
+
+  /**
+   * Logs a message
+   * @param message
+   * @param type
+   * @param context
+   */
+  log(message, type, context) {
+    if (this.isActive()) {
+      const args = [message];
+
+      // Display context in console
+      if (typeof context !== 'undefined' && this.options.displayContext === true) {
+        args.push(context);
+      }
+
+      // Displays the message in the console
+      switch (type) {
+        case Types.debug:
+          if (this.options.console.debug === true) {
+            console.log.apply(this, args);
+          }
+          break;
+
+        case Types.error:
+          if (this.options.console.error === true) {
+            console.error.apply(this, args);
+          }
+          break;
+
+        case Types.info:
+          if (this.options.console.info === true) {
+            console.info.apply(this, args);
+          }
+          break;
+
+        case Types.warning:
+          if (this.options.console.warning === true) {
+            console.warn.apply(this, args);
+          }
+          break;
+
+        default:
+          if ((typeof this.options.console[type] === 'boolean'
+            && this.options.console[type] === true)
+            || (typeof this.options.console[type] !== 'boolean'
+              && this.options.console.other === true)) {
+            console.log.apply(this, args);
+          }
+      }
     }
 
-    /**
-     * Logs a debug message
-     * @param message
-     * @param context
-     */
-    debug(message, context) {
-        return this.log(message, Types.debug, context);
-    }
+    // Notify all listeners
+    this.observer.notify(type, ...[message, context]);
+  }
 
-    /**
-     * Logs an error message
-     * @param message
-     * @param context
-     */
-    error(message, context) {
-        if (message instanceof Error) {
-            context = context || {};
-            context.error = {};
+  /**
+   * Removes an event listener
+   * @param event
+   * @param callback
+   */
+  off(event, callback) {
+    this.observer.detach(event, callback);
+  }
 
-            const attributes = ["name", "message", "reason", "stack", "type"];
+  /**
+   * Adds an event listener
+   * @param event
+   * @param callback
+   */
+  on(event, callback) {
+    this.observer.attach(event, callback);
+  }
 
-            for (let i = 0; i < attributes.length; i += 1) {
-                if (attributes[i] in message) {
-                    context.error[attributes[i]] = message[attributes[i]];
-                }
-            }
-            message = message.message;
-        }
-        return this.log(message, Types.error, context);
-    }
+  /**
+   * Activates or deactivates the logger
+   * @param active
+   */
+  setActive(active) {
+    this.options.active = (active === true);
+  }
 
-    /**
-     * Logs an information message
-     * @param message
-     * @param context
-     */
-    info(message, context) {
-        return this.log(message, Types.info, context);
-    }
-
-    /**
-     * Checks if the logger is active
-     * @return {boolean}
-     */
-    isActive() {
-        return this.options.active === true;
-    }
-
-    /**
-     * Logs a message
-     * @param message
-     * @param type
-     * @param context
-     */
-    log(message, type, context) {
-        if (this.isActive()) {
-            const args = [message];
-
-            // Display context in console
-            if (typeof context !== "undefined" && this.options.displayContext === true) {
-                args.push(context);
-            }
-
-            // Displays the message in the console
-            switch (type) {
-                case Types.debug:
-                    if (this.options.console.debug === true) {
-                        console.debug.apply(this, args);
-                    }
-                    break;
-
-                case Types.error:
-                    if (this.options.console.error === true) {
-                        console.error.apply(this, args);
-                    }
-                    break;
-
-                case Types.info:
-                    if (this.options.console.info === true) {
-                        console.info.apply(this, args);
-                    }
-                    break;
-
-                case Types.warning:
-                    if (this.options.console.warning === true) {
-                        console.warn.apply(this, args);
-                    }
-                    break;
-
-                default:
-                    if ((typeof this.options.console[type] === "boolean"
-                            && this.options.console[type] === true)
-                        || (typeof this.options.console[type] !== "boolean"
-                            && this.options.console.other === true)) {
-                        console.log.apply(this, args);
-                    }
-            }
-        }
-
-        // Notify all listeners
-        this._observer.notify(type, ...[message, context]);
-    }
-
-    /**
-     * Removes an event listener
-     * @param event
-     * @param callback
-     */
-    off(event, callback) {
-        this._observer.detach(event, callback);
-    }
-
-    /**
-     * Adds an event listener
-     * @param event
-     * @param callback
-     */
-    on(event, callback) {
-        this._observer.attach(event, callback);
-    }
-
-    /**
-     * Activates or deactivates the logger
-     * @param active
-     */
-    setActive(active) {
-        this.options.active = (active === true);
-    }
-
-    /**
-     * Logs a warning message
-     * @param message
-     * @param context
-     */
-    warn(message, context) {
-        return this.log(message, Types.warning, context);
-    }
+  /**
+   * Logs a warning message
+   * @param message
+   * @param context
+   */
+  warn(message, context) {
+    return this.log(message, Types.warning, context);
+  }
 }
 
 export default Logger;
--- test/logger.test.js
+++ test/logger.test.js
@@ -15,7 +15,7 @@
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
@@ -23,216 +23,198 @@
  *
  */
 
-import Logger from "../src/index";
-import Types from "../src/types";
+import Logger from '../src/logger';
+import Types from '../src/types';
 
-describe("Logger", () => {
-
-    it(`should be importable from package`, () => {
-        expect(typeof Logger).toEqual("function");
-    });
+describe('Logger', () => {
+  it('should be importable from package', () => {
+    expect(typeof Logger).toEqual('function');
+  });
 });
 
-describe("new Logger({active: true})", () => {
-
-    it(`should create an active logger`, () => {
-        const logger = new Logger({active: true});
-        expect(logger.isActive()).toEqual(true);
-    });
+describe('new Logger({active: true})', () => {
+  it('should create an active logger', () => {
+    const logger = new Logger({ active: true });
+    expect(logger.isActive()).toEqual(true);
+  });
 });
 
-describe("new Logger({active: false})", () => {
-
-    it(`should create an inactive logger`, () => {
-        const logger = new Logger({active: false});
-        expect(logger.isActive()).toEqual(false);
-    });
+describe('new Logger({active: false})', () => {
+  it('should create an inactive logger', () => {
+    const logger = new Logger({ active: false });
+    expect(logger.isActive()).toEqual(false);
+  });
 });
 
-describe("Logger.debug(String, Object)", () => {
-
-    it(`should log a debug message`, () => {
-        let text = null;
-        const msg = "Hello World";
-        const logMessage = "msg = " + msg;
-        const logger = new Logger({active: true});
-        logger.on(Types.debug, (message) => {
-            text = message;
-        });
-        logger.debug(logMessage, {msg: msg});
-        expect(text).toEqual(logMessage);
+describe('Logger.debug(String, Object)', () => {
+  it('should log a debug message', () => {
+    let text = null;
+    const msg = 'Hello World';
+    const logMessage = `msg = ${msg}`;
+    const logger = new Logger({ active: true });
+    logger.on(Types.debug, (message) => {
+      text = message;
     });
+    logger.debug(logMessage, { msg });
+    expect(text).toEqual(logMessage);
+  });
 });
 
-describe("Logger.error(String, Object)", () => {
-
-    it(`should log an error message`, () => {
-        let text = null;
-        const error = new Error("forbidden");
-        const logger = new Logger({active: true});
-        logger.on(Types.error, (message) => {
-            text = message;
-        });
-        logger.error(error.message, {error: error});
-        expect(text).toEqual(error.message);
+describe('Logger.error(String, Object)', () => {
+  it('should log an error message', () => {
+    let text = null;
+    const error = new Error('forbidden');
+    const logger = new Logger({ active: true });
+    logger.on(Types.error, (message) => {
+      text = message;
     });
+    logger.error(error.message, { error });
+    expect(text).toEqual(error.message);
+  });
 });
 
-describe("Logger.error(Error, Object)", () => {
-
-    it(`should log an error message`, () => {
-        let text = null;
-        const error = new Error("forbidden");
-        const logger = new Logger({active: true});
-        logger.on(Types.error, (message) => {
-            text = message;
-        });
-        logger.error(error, {error: error});
-        expect(text).toEqual(error.message);
+describe('Logger.error(Error, Object)', () => {
+  it('should log an error message', () => {
+    let text = null;
+    const error = new Error('forbidden');
+    const logger = new Logger({ active: true });
+    logger.on(Types.error, (message) => {
+      text = message;
     });
+    logger.error(error, { error });
+    expect(text).toEqual(error.message);
+  });
 });
 
-describe("Logger.info(String, Object)", () => {
-
-    it(`should log an info message`, () => {
-        let text = null;
-        const logMessage = "hello";
-        const logger = new Logger({active: true});
-        logger.on(Types.info, (message) => {
-            text = message;
-        });
-        logger.info(logMessage);
-        expect(text).toEqual(logMessage);
+describe('Logger.info(String, Object)', () => {
+  it('should log an info message', () => {
+    let text = null;
+    const logMessage = 'hello';
+    const logger = new Logger({ active: true });
+    logger.on(Types.info, (message) => {
+      text = message;
     });
+    logger.info(logMessage);
+    expect(text).toEqual(logMessage);
+  });
 });
 
-describe("Logger.info(String, Object)", () => {
-
-    it(`should log a warning message`, () => {
-        let text = null;
-        const logMessage = "warning";
-        const logger = new Logger({active: true});
-        logger.on(Types.warning, (message) => {
-            text = message;
-        });
-        logger.warn(logMessage);
-        expect(text).toEqual(logMessage);
+describe('Logger.info(String, Object)', () => {
+  it('should log a warning message', () => {
+    let text = null;
+    const logMessage = 'warning';
+    const logger = new Logger({ active: true });
+    logger.on(Types.warning, (message) => {
+      text = message;
     });
+    logger.warn(logMessage);
+    expect(text).toEqual(logMessage);
+  });
 });
 
-describe("Logger.isActive()", () => {
+describe('Logger.isActive()', () => {
+  it('should return true if the logger is active', () => {
+    const logger = new Logger({ active: true });
+    expect(logger.isActive()).toEqual(true);
+  });
 
-    it(`should return true if the logger is active`, () => {
-        const logger = new Logger({active: true});
-        expect(logger.isActive()).toEqual(true);
-    });
-
-    it(`should return true if the logger is not active`, () => {
-        const logger = new Logger({active: false});
-        expect(logger.isActive()).toEqual(false);
-    });
+  it('should return true if the logger is not active', () => {
+    const logger = new Logger({ active: false });
+    expect(logger.isActive()).toEqual(false);
+  });
 });
 
-describe("Logger.log(String, String, Object)", () => {
-
-    it(`should log a message`, () => {
-        let text = null;
-        const msg = "message";
-        const logger = new Logger({active: true});
-        logger.on(Types.info, (message) => {
-            text = message;
-        });
-        logger.log(msg, Types.info, {msg: msg});
-        expect(text).toEqual(msg);
+describe('Logger.log(String, String, Object)', () => {
+  it('should log a message', () => {
+    let text = null;
+    const msg = 'message';
+    const logger = new Logger({ active: true });
+    logger.on(Types.info, (message) => {
+      text = message;
     });
+    logger.log(msg, Types.info, { msg });
+    expect(text).toEqual(msg);
+  });
 
-    it(`should log a message with context`, () => {
-        let context = null;
-        const logger = new Logger({active: true});
-        logger.on(Types.info, (message, ctx) => {
-            context = ctx;
-        });
-        logger.log("message", Types.info, {a: 1});
-        expect(context).toEqual({a: 1});
+  it('should log a message with context', () => {
+    let context = null;
+    const logger = new Logger({ active: true });
+    logger.on(Types.info, (message, ctx) => {
+      context = ctx;
     });
+    logger.log('message', Types.info, { a: 1 });
+    expect(context).toEqual({ a: 1 });
+  });
 });
 
-describe(`Logger.on(String, Function)`, () => {
+describe('Logger.on(String, Function)', () => {
+  describe(`Logger.on("${Types.debug}", Function)`, () => {
+    it(`should execute a callback for ${Types.debug} messages`, () => {
+      let changed = false;
+      const logger = new Logger();
 
-    describe(`Logger.on("${Types.debug}", Function)`, () => {
-
-        it(`should execute a callback for ${Types.debug} messages`, () => {
-            let changed = false;
-            const logger = new Logger();
-
-            logger.on(Types.debug, () => {
-                changed = true;
-            });
-            logger.debug("this is a debug message");
-            expect(changed).toEqual(true);
-        });
+      logger.on(Types.debug, () => {
+        changed = true;
+      });
+      logger.debug('this is a debug message');
+      expect(changed).toEqual(true);
     });
+  });
 
-    describe(`Logger.on("${Types.error}", Function)`, () => {
+  describe(`Logger.on("${Types.error}", Function)`, () => {
+    it(`should execute a callback for ${Types.error} messages`, () => {
+      let changed = false;
+      const logger = new Logger();
 
-        it(`should execute a callback for ${Types.error} messages`, () => {
-            let changed = false;
-            const logger = new Logger();
-
-            logger.on(Types.error, () => {
-                changed = true;
-            });
-            logger.error("this is an error message");
-            expect(changed).toEqual(true);
-        });
+      logger.on(Types.error, () => {
+        changed = true;
+      });
+      logger.error('this is an error message');
+      expect(changed).toEqual(true);
     });
+  });
 
-    describe(`Logger.on("${Types.info}", Function)`, () => {
+  describe(`Logger.on("${Types.info}", Function)`, () => {
+    it(`should execute a callback for ${Types.info} messages`, () => {
+      let changed = false;
+      const logger = new Logger();
 
-        it(`should execute a callback for ${Types.info} messages`, () => {
-            let changed = false;
-            const logger = new Logger();
-
-            logger.on(Types.info, () => {
-                changed = true;
-            });
-            logger.info("this is an information message");
-            expect(changed).toEqual(true);
-        });
+      logger.on(Types.info, () => {
+        changed = true;
+      });
+      logger.info('this is an information message');
+      expect(changed).toEqual(true);
     });
+  });
 
-    describe(`Logger.on("${Types.warning}", Function)`, () => {
+  describe(`Logger.on("${Types.warning}", Function)`, () => {
+    it(`should execute a callback for ${Types.warning} messages`, () => {
+      let changed = false;
+      const logger = new Logger();
 
-        it(`should execute a callback for ${Types.warning} messages`, () => {
-            let changed = false;
-            const logger = new Logger();
-
-            logger.on(Types.warning, () => {
-                changed = true;
-            });
-            logger.warn("this is a warning message");
-            expect(changed).toEqual(true);
-        });
+      logger.on(Types.warning, () => {
+        changed = true;
+      });
+      logger.warn('this is a warning message');
+      expect(changed).toEqual(true);
     });
+  });
 });
 
-describe("Logger.setActive(Boolean)", () => {
-
-    describe("Logger.setActive(true)", () => {
-
-        it(`should activate a logger`, () => {
-            const logger = new Logger({active: false});
-            logger.setActive(true);
-            expect(logger.isActive()).toEqual(true);
-        });
+describe('Logger.setActive(Boolean)', () => {
+  describe('Logger.setActive(true)', () => {
+    it('should activate a logger', () => {
+      const logger = new Logger({ active: false });
+      logger.setActive(true);
+      expect(logger.isActive()).toEqual(true);
     });
+  });
 
-    describe("Logger.setActive(false)", () => {
-
-        it(`should deactivate a logger`, () => {
-            const logger = new Logger({active: true});
-            logger.setActive(false);
-            expect(logger.isActive()).toEqual(false);
-        });
+  describe('Logger.setActive(false)', () => {
+    it('should deactivate a logger', () => {
+      const logger = new Logger({ active: true });
+      logger.setActive(false);
+      expect(logger.isActive()).toEqual(false);
     });
+  });
 });

2e6ddc5efc21404557dd98eb5e0ccea73fb03dcb
Karl Stein <karl.stein.pro@gmail.com>
2023-05-01 10:08:42 -1000

    Added `fileOutput()` to save logs to a local file (NodeJS only)

---
+++ src/outputs/fileOutput.ts
@@ -0,0 +1,76 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2023 Karl STEIN
+ */
+
+import * as fs from 'fs';
+import { defaultFormatter } from '../formatter';
+import { LogEvent, LogEventContext } from '../util';
+
+interface FileOutputOptions {
+  /**
+   * The number of milliseconds between each flush to the file.
+   */
+  flushInterval?: number;
+  /**
+   * The function used to format message before saving to file.
+   * @param event
+   */
+  formatter?: (event: LogEvent<LogEventContext>) => string;
+  /**
+   * Characters used to separate each log.
+   */
+  lineSeparator?: string;
+  /**
+   * The logs file path.
+   */
+  path: string;
+}
+
+/**
+ * Log events to a file.
+ * @param options
+ */
+function fileOutput(options: FileOutputOptions): (ev: LogEvent<LogEventContext>) => void {
+  const {
+    flushInterval,
+    formatter,
+    lineSeparator,
+    path,
+  } = {
+    formatter: defaultFormatter,
+    lineSeparator: '\r\n',
+    ...options,
+  };
+
+  if (path == null || path == '') {
+    throw new Error('path option must not be null or empty');
+  }
+
+  let buffer = '';
+
+  if (flushInterval) {
+    setInterval(() => {
+      if (buffer.length > 0) {
+        // Write pending logs.
+        fs.appendFileSync(path, buffer, { encoding: 'utf-8' });
+        buffer = '';
+      }
+    }, flushInterval);
+  }
+
+  return (event) => {
+    const message = formatter(event);
+    const line = `${message}${lineSeparator}`;
+
+    if (flushInterval) {
+      // Put log in buffer.
+      buffer += line;
+    } else {
+      // Write log now.
+      fs.appendFileSync(path, line, { encoding: 'utf-8' });
+    }
+  };
+}
+
+export default fileOutput;
---
+++ test/outputs/fileOutput.test.ts
@@ -0,0 +1,105 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2023 Karl STEIN
+ */
+
+import { afterAll, describe, expect, it, jest } from '@jest/globals';
+import * as fs from 'fs';
+import { INFO } from '../../src';
+import fileOutput from '../../src/outputs/fileOutput';
+import { LogEvent, LogEventContext } from '../../src/util';
+
+// Replace setInterval() with fake timer.
+jest.useFakeTimers();
+jest.spyOn(global, 'setInterval');
+
+const formatter = JSON.stringify;
+const lineSeparator = '\n';
+const path = 'logs.txt';
+
+function getLastEntry(content: string): string {
+  return content.split(lineSeparator).filter((v) => v !== '').pop();
+}
+
+afterAll(async () => {
+  try {
+    await fs.promises.unlink(path);
+  } catch (e) {
+    // do nothing
+  }
+});
+
+function createEvent(): LogEvent<LogEventContext> {
+  return {
+    timestamp: Date.now(),
+    logger: 'test',
+    level: INFO,
+    message: 'This is a message',
+    context: { random: Math.random().toString() },
+  };
+}
+
+describe('fileOutput({ path: null | undefined })', () => {
+  it('should throw an error', () => {
+    expect(() => {
+      fileOutput({ path: null });
+    }).toThrow();
+
+    expect(() => {
+      fileOutput({ path: undefined });
+    }).toThrow();
+  });
+});
+
+describe('fileOutput({ path: "logs.txt" })(event)', () => {
+  const output = fileOutput({ formatter, lineSeparator, path });
+
+  const event = createEvent();
+  output(event);
+
+  it('should create the logs file', () => {
+    expect(async () => {
+      await fs.promises.stat(path);
+    }).not.toThrow();
+  });
+
+  it('should write event to the file', async () => {
+    const logs = await fs.promises.readFile(path, { encoding: 'utf-8' });
+    const lastEntry = getLastEntry(logs);
+    expect(lastEntry).toBe(formatter(event));
+  });
+});
+
+describe('fileOutput({ path: "logs.txt", flushInterval: number })(event)', () => {
+  const flushInterval = 1000;
+  const output = fileOutput({
+    formatter,
+    flushInterval,
+    lineSeparator,
+    path,
+  });
+
+  it('should call setInterval() with flushInterval as interval', () => {
+    // jest.runAllTimers();
+    // jest.runOnlyPendingTimers();
+    expect(setInterval).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not write events to the file before the flushInterval', async () => {
+    const event = createEvent();
+    output(event);
+    jest.advanceTimersByTime(Math.round(flushInterval / 2));
+    const logs = await fs.promises.readFile(path, { encoding: 'utf-8' });
+    const lastEntry = getLastEntry(logs);
+    expect(lastEntry).not.toBe(formatter(event));
+  });
+
+  it('should write events to the file after the flushInterval', async () => {
+    const event = { ...createEvent(), message: 'xxx' };
+    output(event);
+    jest.advanceTimersByTime(flushInterval * 2);
+    const logs = await fs.promises.readFile(path, { encoding: 'utf-8' });
+    const lastEntry = getLastEntry(logs);
+    expect(lastEntry).toBe(formatter(event));
+  });
+});
